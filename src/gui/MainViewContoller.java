d4c0ff155f89c361fca68f7db2f7f4aac3aa794d
raphael carlos <raphaelpereiracarlos@gmail.com>
2020-09-06 10:08:30 -0300

    DepartmentList view design

---
+++ src/gui/MainViewContoller.java
@@ -0,0 +1,73 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class MainViewContoller implements Initializable {
+
+	// Itens de controle de tela
+	@FXML
+	private MenuItem menuItemSeller;
+
+	@FXML
+	private MenuItem menuItemDepartment;
+
+	@FXML
+	private MenuItem menuItemAbout;
+
+	// Metodos para tratar os itens de menu
+
+	@FXML
+	public void onMenuItemSellerAction() {
+		System.out.println("onMenuItemSellerAction");
+	}
+
+	@FXML
+	public void onMenuItemDepartmentAction() {
+		loadView("/gui/DepartmentList.fxml");
+	}
+
+	@FXML
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml");
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+
+	}
+
+	private void loadView(String absoluteName) {
+
+		try {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+}
